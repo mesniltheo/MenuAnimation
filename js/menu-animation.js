edb596c2a3388b9bef65a4a3c6f54b198819c52b
MESNIL Theo <mesniltheo@gmail.com>
2016-04-10 22:07:39 +0200

    Create imports files & defaults values

---
+++ js/menu-animation.js
@@ -0,0 +1,18 @@
+
+
+(function() {
+  navigations = document.querySelectorAll('.navigation');
+  [].forEach.call(navigations, function(navigation) {
+    [].forEach.call(navigation.children, function(list) {
+      currentLi = function(e) {
+        e.preventDefault();
+        if (list.classList.contains('active')) {
+          return false;
+        }
+        navigation.querySelector('.active').classList.remove('active');
+        this.classList.add('active');
+      };
+      list.addEventListener('click', currentLi);
+    });
+  });
+})(window);
